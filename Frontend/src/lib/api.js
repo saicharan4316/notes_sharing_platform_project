da0de4e2326bfc7b2eb483cf3feeaaa3648655a2
sai charan <utukurisaicharan123@gmail.com>
2026-01-03 14:45:43 +0530

    changed .env variable name

--- Frontend/src/lib/api.js
+++ Frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL_3000 || "https://notes-sharing-platform-project.onrender.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://notes-sharing-platform-project.onrender.com",
   withCredentials: false,
   timeout: 20000,
 });
